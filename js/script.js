474c26ddee563ef97f2cb91fe8a510bc2f326f35
Danilo <danilo.colloca91@gmail.com>
2020-12-15 17:39:18 +0100

    modifica ricerca all'interno dell'array

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
 
 //il prompt richiede la mail
+
 var insMail = prompt('inserisci la tua mail: suggerimento(tuonome@boolean.com)');
 
 // array contenente le mail valide
+
 var mailValide = ['enrico@boolean.com', 'alfredo@boolean.com', 'ottavio@boolean.com'];
 
 // cerco nell'array la corrispondenza tra le mail
-for (var i = 0; i < mailValide.length; i++) {
-  if (insMail != mailValide[i]) {
-    document.getElementById('errormsg').innerHTML = 'Errore la mail inserita non è valida!' //se la mail non corrisponde error msg
+var mailChecked = false;
 
+for (var i = 0; i <= mailValide.length; i++) {
+  if (insMail === mailValide[i]) {
+    mailChecked = true;
   }
-  
+
+}
+
+if (!mailChecked) {
+  alert('Errore la mail inserita non è valida!') //se la mail non corrisponde error msg
+}else {
   //se la mail corrisponde msg di benvenuto con nome utente personalizzato
+
   if (insMail === mailValide[0]) {
-    document.getElementById('errormsg').innerHTML = 'Ciao Enrico!'
+    document.getElementById('errormsg').innerHTML = 'Ciao Enrico iniziamo a giocare!'
   }
   if (insMail === mailValide[1]) {
-    document.getElementById('errormsg').innerHTML = 'Ciao Alfredo!'
+    document.getElementById('errormsg').innerHTML = 'Ciao Alfredo iniziamo a giocare!'
   }if (insMail === mailValide[2]) {
-    document.getElementById('errormsg').innerHTML = 'Ciao Ottavio!'
+    document.getElementById('errormsg').innerHTML = 'Ciao Ottavio iniziamo a giocare!'
   }
+
 }
